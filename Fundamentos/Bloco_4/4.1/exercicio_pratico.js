db8ce50760c1e26312ae9a65776cede3e3948fb3
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-02-27 20:08:46 -0300

    resolução do quesito 1

---
+++ Fundamentos/Bloco_4/4.1/exercicio_pratico.js
@@ -0,0 +1,28 @@
+//1. Criação de cinco programas, um para cada operação aritmética básica, com duas variáveis, A e B.
+let a = 10;
+let b = 5;
+
+//Adição
+if (a > b) {
+    console.log(a + b);
+}
+
+//Subtração
+if (a > b) {
+    console.log(a - b);
+}
+
+//Multiplicação
+if (a > b) {
+    console.log(a * b);
+}
+
+//Divisão
+if (a > b) {
+    console.log(a / b);
+}
+
+//Módulo
+if (a > b) {
+    console.log(a % b);
+}
