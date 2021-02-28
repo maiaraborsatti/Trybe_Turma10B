463fc5bb83618b38be6878d86f54e252ac636d40
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-02-28 20:09:48 -0300

    exercícios - arrays

--- Fundamentos/Bloco_4/4.2/exercicio_fixacao.js
+++ Fundamentos/Bloco_4/4.2/exercicio_fixacao.js
@@ -14,3 +14,20 @@ console.log(tasksList); // ['Tomar café', 'Reunião', 'Brincar com o cachorro']
 
 let indexOfTask = tasksList.indexOf('Reunião'); //procura o índice de um item no Array
 console.log(indexOfTask); //1
+
+//---------------------------------------------------------
+//Exercício 1 - Obtenha o valor "Serviços" do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//Exercício 2 - Procure o índice do valor "Portfólio" do array menu:
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Exercício 3 - Adicione o valor "Contato" no final do array menu:
+menu.push('Contato');
+
+console.log(menu);
