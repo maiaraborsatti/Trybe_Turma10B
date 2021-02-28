d93b40d620e038c687ab52cdae6b5646e0dd027a
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-02-28 09:30:31 -0300

    resolução do quesito 7

--- Fundamentos/Bloco_4/4.1/exercicio_pratico.js
+++ Fundamentos/Bloco_4/4.1/exercicio_pratico.js
@@ -91,5 +91,36 @@ switch (peca.toLowerCase()) {
 
     default:
     console.log("Essa peça não existe no jogo!")
+}
 
+//7. Criar  um programa que converte uma nota dada em porcentagem (de 0 a 100).
+let porcentagem = 73;
+
+switch (true) {
+    case (porcentagem >= 90 && porcentagem <= 100):
+        console.log("A");
+        break;
+
+    case (porcentagem >= 80 && porcentagem < 90):
+        console.log("B");
+        break;
+        
+    case (porcentagem >= 70 && porcentagem < 80):
+        console.log("C");
+        break;
+
+    case (porcentagem >= 60 && porcentagem < 70):
+        console.log("D");
+        break;
+    
+    case (porcentagem >= 50 && porcentagem < 60):
+        console.log("E");
+        break;
+    
+    case (porcentagem < 50 && porcentagem > 0):
+        console.log("F");
+        break;
+
+    default:
+    console.log("Erro")
 }
