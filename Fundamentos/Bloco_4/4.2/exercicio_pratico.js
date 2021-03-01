e68e906c538bc0113eb8a6fe5bfae8d2203ddac8
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-02-28 21:29:39 -0300

    exercícios práticos - 1

---
+++ Fundamentos/Bloco_4/4.2/exercicio_pratico.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1. Percorrer o array imprimindo todos os valores nele contidos com a função console.log()
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+  }
