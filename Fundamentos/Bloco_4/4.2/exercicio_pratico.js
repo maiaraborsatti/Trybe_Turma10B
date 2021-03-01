7555f0e06f625985809563ff8a6c740cbc7d500e
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-02-28 21:34:48 -0300

    exercícios práticos - 2

--- Fundamentos/Bloco_4/4.2/exercicio_pratico.js
+++ Fundamentos/Bloco_4/4.2/exercicio_pratico.js
@@ -3,5 +3,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //1. Percorrer o array imprimindo todos os valores nele contidos com a função console.log()
 
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i])
+    console.log(numbers[i]);
   }
+
+//2. Somar todos os valores contidos no array e imprimir o resultado
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    soma = numbers[i] + soma;
+  }
+console.log(soma);
