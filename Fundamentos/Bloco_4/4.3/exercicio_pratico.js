1a184689db2e8c4435df6463316007d2ba9337f2
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-01 19:26:12 -0300

    exercícios práticos - 2

--- Fundamentos/Bloco_4/4.3/exercicio_pratico.js
+++ Fundamentos/Bloco_4/4.3/exercicio_pratico.js
@@ -1,11 +1,21 @@
-//1. Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n
+//1. Fazer um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n
 let n = 5;
 let icone = '*';
-let linha = '';
+let conteudoLinha = '';
 
 for (let qtdIcone = 0; qtdIcone < n; qtdIcone += 1) {
-  linha = linha + icone;
+  conteudoLinha = conteudoLinha + icone;
 };
 for (let qtdLinha = 0; qtdLinha < n; qtdLinha += 1) {
-  console.log(linha);
+  console.log(conteudoLinha);
 };
+
+//2. Fazer o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
+let conteudoLinha2 = '';
+
+for (let qtdLinha = 0; qtdLinha <= n; qtdLinha += 1) {
+    console.log(conteudoLinha2);
+    conteudoLinha2 = conteudoLinha2 + icone
+  };
+
+//3. 
