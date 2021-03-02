dc219e48ca0de0ddb904c21e8d10e1ade0c454a2
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-02 10:21:34 -0300

    exercícios práticos - 3

--- Fundamentos/Bloco_4/4.3/exercicio_pratico.js
+++ Fundamentos/Bloco_4/4.3/exercicio_pratico.js
@@ -11,11 +11,30 @@ for (let qtdLinha = 0; qtdLinha < n; qtdLinha += 1) {
 };
 
 //2. Fazer o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
+let n2 = 5;
+let icone2 = '*';
 let conteudoLinha2 = '';
 
-for (let qtdLinha = 0; qtdLinha <= n; qtdLinha += 1) {
+for (let qtdLinha = 0; qtdLinha <= n2; qtdLinha += 1) {
     console.log(conteudoLinha2);
-    conteudoLinha2 = conteudoLinha2 + icone
+    conteudoLinha2 = conteudoLinha2 + icone2
   };
 
-//3. 
+//3. Inverta o lado do triângulo
+let n3 = 5;
+let icone3 = '*';
+let conteudoLinha3 = '';
+let preencheIcone = n3;
+
+for (let qtdLinha = 0; qtdLinha < n3; qtdLinha += 1) {
+  for (let preencheLinha = 0; preencheLinha <= n3; preencheLinha += 1) {
+    if (preencheLinha < preencheIcone) {
+      conteudoLinha3 = conteudoLinha3 + ' ';
+    } else {
+      conteudoLinha3 = conteudoLinha3 + icone3;
+    } //faz o looping formando cada linha
+  }
+  console.log(conteudoLinha3); //imprime a linha formada acima
+  conteudoLinha3 = ''; //zera a linha para recomeçar a nova com mais um *
+  preencheIcone = preencheIcone - 1; //diminui 1 nº a cada repetição
+};
