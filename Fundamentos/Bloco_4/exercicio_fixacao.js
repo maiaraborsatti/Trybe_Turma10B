1365884c5cbe432f6a780f69cafe268ab4a5dd05
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-02 10:50:09 -0300

    exercícios de fixação - 2

--- Fundamentos/Bloco_4/exercicio_fixacao.js
+++ Fundamentos/Bloco_4/exercicio_fixacao.js
@@ -6,3 +6,5 @@ let player = {
   age: 34,
   medals: { golden: 2, silver: 3 },
 }
+ //2. Concatenar as informações para fazer um console.log
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
