3071a1df8ebad13d723f1add5f18a59089c8e309
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-02 10:55:21 -0300

    exercícios de fixação - 3

--- Fundamentos/Bloco_4/exercicio_fixacao.js
+++ Fundamentos/Bloco_4/exercicio_fixacao.js
@@ -8,3 +8,7 @@ let player = {
 }
  //2. Concatenar as informações para fazer um console.log
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
+
+//3. Adicionar ao objeto a chave bestInTheWorld e atribua a esta chave um array
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
