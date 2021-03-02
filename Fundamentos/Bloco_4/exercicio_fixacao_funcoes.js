baac840d808fc63a5230092eb6ab46296aae2615
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-02 11:28:59 -0300

    exercícios de fixação: funções - 1

---
+++ Fundamentos/Bloco_4/exercicio_fixacao_funcoes.js
@@ -0,0 +1,56 @@
+//Fazer com que todos modelos abaixo sejam funções:
+
+//1. Criação de cinco programas, um para cada operação aritmética básica, com duas variáveis, A e B.
+let a = 10;
+let b = 5;
+let c = 15;
+
+//Adição
+if (a > b) {
+    console.log(a + b);
+}
+
+function sum(a, b) {
+    return a + b;
+  }
+console.log(sum(a, b))
+
+//Subtração
+if (a > b) {
+    console.log(a - b);
+}
+
+function sub(a, b) {
+    return a - b;
+  }
+console.log(sub(a, b))
+
+//Multiplicação
+if (a > b) {
+    console.log(a * b);
+}
+
+function mult(a, b) {
+    return a * b;
+  }
+console.log(mult(a, b))
+
+//Divisão
+if (a > b) {
+    console.log(a / b);
+}
+
+function div(a, b) {
+    return a / b;
+  }
+console.log(div(a, b))
+
+//Módulo
+if (a > b) {
+    console.log(a % b);
+}
+
+function mod(a, b) {
+    return a % b;
+  }
+console.log(mod(a, b))
