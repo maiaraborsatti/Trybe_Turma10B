e5eb308e6bd8cd2abc27eefd0163f0dc91ac7032
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-24 17:59:37 -0300

    Parte II

--- Fundamentos/Bloco_7/7.2/exercicio_fixacao.js
+++ Fundamentos/Bloco_7/7.2/exercicio_fixacao.js
@@ -15,3 +15,34 @@ novaChave = 'nomeCompleto';
 const nomeCompleto = `${cliente.nome} ${cliente.sobrenome}`;
 cliente[novaChave] = nomeCompleto;
 console.log(cliente);
+
+// Parte II - Object.keys
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+// Tente criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade"
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for(index in arrayOfSkills){
+    console.log(`Habilidade: ${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
