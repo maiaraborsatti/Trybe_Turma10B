4388992360baa7963f90204a8d137ca68982073a
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-24 18:24:01 -0300

    Parte V

--- Fundamentos/Bloco_7/7.2/exercicio_fixacao.js
+++ Fundamentos/Bloco_7/7.2/exercicio_fixacao.js
@@ -86,7 +86,25 @@ for(index in pairKeyValue) {
   console.log('Capital:', pairKeyValue[index][1]);
 };
 
+// Parte V - Object.assign
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const clone = Object.assign(person, lastName);
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+console.log(lastName);
 
 
+const newPerson = Object.assign({},person,lastName); // { lastName: 'Silva' }
+newPerson.name = 'Gilberto';
+console.log(newPerson); // { name: 'Gilberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
 
 
