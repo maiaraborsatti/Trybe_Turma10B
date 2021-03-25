084734b7501c9ea4b220a30e46f3c13ce3ffa524
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 18:45:03 -0300

    requisito 1 - alteraçao

--- Fundamentos/Bloco_7/7.3/exercicio_pratico.js
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico.js
@@ -1,2 +1,8 @@
+const assert = require('assert');
+
 // 1. A função sum(a, b) retorna a soma do parâmetro a com o b
-const sum = (a, b) => a + b;
+const sum = () => {};
+
+assert.strictEqual(typeof sum, 'function')
+
+
