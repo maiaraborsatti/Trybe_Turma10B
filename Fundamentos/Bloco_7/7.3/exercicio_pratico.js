eb0e96e11a6ed12450f1b9bbda7e70522e50b367
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 19:08:57 -0300

    requisito 4 - correçao e requisito 5

--- Fundamentos/Bloco_7/7.3/exercicio_pratico.js
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico.js
@@ -16,4 +16,7 @@ assert.strictEqual(sum(2, 3), 5);
 assert.strictEqual(sum(0, 0), 0);
 
 // 4. Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+assert.throws(() => { sum(4, "5"); });
+
+// 5. Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
 assert.throws(() => { sum(4, "5"); }, /^Error: parameters must be numbers$/);
