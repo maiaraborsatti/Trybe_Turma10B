0269e003aacde12bf6b37398e8e830ee910ca388
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 18:53:03 -0300

    requisito 2

--- Fundamentos/Bloco_7/7.3/exercicio_pratico.js
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico.js
@@ -4,8 +4,8 @@ const sum = (a, b) => a + b;
 // Verifica se é uma função:
 assert.strictEqual(typeof sum, 'function');
 
-// 1. A função sum(a, b) retorna a soma do parâmetro a com o b
+// 1. A função sum(a, b) retorna a soma do parâmetro a com o b:
+assert.strictEqual(sum(2, 3), 5);
 
-
-
-
+// 3. Teste se o retorno de sum(0, 0) é 0:
+assert.strictEqual(sum(0, 0), 0);
