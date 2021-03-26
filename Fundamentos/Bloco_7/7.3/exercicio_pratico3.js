a784730886608ccf497210c13e8c8230d82c5fa2
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 21:56:34 -0300

    requisito 1 - Parte 3

---
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico3.js
@@ -0,0 +1,21 @@
+// A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
+
+const assert = require('assert');
+
+const myRemoveWithoutCopy = (arr, item) => {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) { 
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// Verifica se é uma função:
+assert.strictEqual(typeof myRemoveWithoutCopy, 'function');
+
+// 1. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
