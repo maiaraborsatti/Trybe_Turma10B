53707ad97f00493cc868ca228613c64da5108330
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 21:15:48 -0300

    requisito 3

--- Fundamentos/Bloco_7/7.3/exercicio_pratico2.js
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico2.js
@@ -20,3 +20,7 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 
 // 2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// 3. Verifique se o array passado por parâmetro não sofreu alteraçõe
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 8), [1, 2, 3, 4]);
+
