2e05c24956bfbe45dbfe3e0c1bcfbaa469e39892
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 22:59:39 -0300

    requisito 5 - Parte 4

--- Fundamentos/Bloco_7/7.3/exercicio_pratico4.js
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico4.js
@@ -9,13 +9,16 @@ function myFizzBuzz(num) {
 }
 
 // 1. Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
-assert.strictEqual(myFizzBuzz(15),'fizzbuzz');
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
 
 // 2. Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
-assert.strictEqual(myFizzBuzz(3),'fizz');
+assert.strictEqual(myFizzBuzz(3), 'fizz');
 
 // 3. Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
-assert.strictEqual(myFizzBuzz(5),'buzz');
+assert.strictEqual(myFizzBuzz(5), 'buzz');
 
 // 4. Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
-assert.strictEqual(myFizzBuzz(7),7);
+assert.strictEqual(myFizzBuzz(7), 7);
+
+// 5.Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(' '), false);
