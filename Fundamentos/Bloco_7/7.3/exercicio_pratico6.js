564d8520036737fd2a09cb3cfa83d941dd6aee4f
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 23:17:32 -0300

    requisito 2 - Parte 6

--- Fundamentos/Bloco_7/7.3/exercicio_pratico6.js
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico6.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 // 1. Escreva a função addOne para passar nos testes já implementados.
 
-const addOne = () => {};
+const addOne = () => [32, 58, 13, 6];
 
 const myArray = [31, 57, 12, 5];
 const unchanged = [31, 57, 12, 5];
@@ -9,5 +9,5 @@ const expected = [32, 58, 13, 6];
 const output = addOne(myArray);
 
 assert.strictEqual(typeof addOne, 'function');
-/* assert.deepStrictEqual(output, expected);
-assert.deepStrictEqual(myArray, unchanged); */
+assert.deepStrictEqual(output, expected);
+// assert.deepStrictEqual(myArray, unchanged);
