7638519df6f71e8eb66b52eaf0ee935a0e6e18eb
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-25 22:45:50 -0300

    requisito 3 - Parte 4

--- Fundamentos/Bloco_7/7.3/exercicio_pratico3.js
+++ Fundamentos/Bloco_7/7.3/exercicio_pratico3.js
@@ -25,3 +25,6 @@ assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 
 // 3. Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 8), [1, 2, 3, 4]);
+
+// 4. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
