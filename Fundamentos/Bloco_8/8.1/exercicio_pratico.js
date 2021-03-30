ffccacb231390c0853f9d0424d60217c89749879
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-29 22:03:26 -0300

    exercicio 3

--- Fundamentos/Bloco_8/8.1/exercicio_pratico.js
+++ Fundamentos/Bloco_8/8.1/exercicio_pratico.js
@@ -27,3 +27,16 @@ const resultado = (aposta,numero) => {
 }
 
 console.log(resultado(2,numero));
+
+/* 3. Crie uma HOF utilizando o conceito de currying. Passe como parâmetros a resposta correta (Ex: Gabarito) para uma pergunta e uma resposta a ser validada (Ex: Resposta de uma pessoa). Sua HOF deve checar se a resposta enviada pela pessoa usuária é correta. Se for, a função retorna true , se não for, a função retorna false .
+- A validação da resposta não deve ser case sensitive.
+- Deve ser possível chamar a HOF criada de forma encadeada (Ex: checarResposta('Gabarito')('Teste')) */
+
+const check = (gabarito) => (resposta) => 
+gabarito.toLowerCase() === resposta.toLowerCase() ? true : false;
+
+// Sugestão de respostas a serem validadas.
+const correctAnswer = 'higher order function';
+const userAnswer = 'HIGHER ORDER FUNCTION';
+
+console.log(check(userAnswer)(correctAnswer));
