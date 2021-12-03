1fe90bf3b3a57d09451b7e77176f740f4ee9b0e2
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-12-03 14:45:33 -0300

    testanto as funçeos

--- Ciência_da_Computação/32.1/area.py
+++ Ciência_da_Computação/32.1/area.py
@@ -14,3 +14,8 @@ def rectangle(length, width):
 def circle(radius):
     """Calculate area of circle."""
     return PI * radius * radius
+
+
+print("Área do quadrado:", square(10))
+print("Área do retângulo:", rectangle(2, 2))
+print("Área do círculo:", circle(3))
