e6a6722ce196bd06c24818fd5b55c17f52448b7f
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-12-03 21:10:13 -0300

    escrita e leitura em bytes

--- Ciência_da_Computação/32.2/manipulacao.py
+++ Ciência_da_Computação/32.2/manipulacao.py
@@ -46,3 +46,16 @@ for line in file:
     print(line)
     # não esqueça que a quebra de linha também é um caractere da linha
 file.close()  # não podemos esquecer de fechar o arquivo
+
+
+# escrita
+file = open("arquivo.dat", mode="wb")
+file.write(b"C\xc3\xa1ssio 30")
+# o prefixo b em uma string indica que seu valor está codificado em bytes
+file.close()
+
+# leitura
+file = open("arquivo.dat", mode="rb")
+content = file.read()
+print(content)  # saída: b'C\xc3\xa1ssio 30'
+file.close()  # não podemos esquecer de fechar o arquivo
