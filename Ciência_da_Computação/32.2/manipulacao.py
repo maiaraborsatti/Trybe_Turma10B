23a93d71d9898a42a5c47c8641cab1248f6298a0
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-12-03 21:08:28 -0300

    escrita e leitura em linhas

--- Ciência_da_Computação/32.2/manipulacao.py
+++ Ciência_da_Computação/32.2/manipulacao.py
@@ -32,3 +32,17 @@ file = open("arquivo.txt", mode="r")
 content = file.read()
 print(content)
 file.close()  # não podemos esquecer de fechar o arquivo
+
+
+# escrita
+file = open("arquivo2.txt", mode="w")
+LINES = ["Olá\n", "mundo\n", "belo\n", "do\n", "Python\n"]
+file.writelines(LINES)
+file.close()
+
+# leitura
+file = open("arquivo2.txt", mode="r")
+for line in file:
+    print(line)
+    # não esqueça que a quebra de linha também é um caractere da linha
+file.close()  # não podemos esquecer de fechar o arquivo
