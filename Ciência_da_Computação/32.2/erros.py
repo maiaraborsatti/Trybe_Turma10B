1220b76bf036326ea51f7dc872c316d6e27112c5
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-12-03 21:19:34 -0300

    testando erros em manipulaçao de arquivos

--- Ciência_da_Computação/32.2/erros.py
+++ Ciência_da_Computação/32.2/erros.py
@@ -4,3 +4,17 @@ while True:
         break
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
+
+
+try:
+    arquivo = open("arquivo.txt", "r")
+except OSError:
+    # será executado caso haja uma exceção
+    print("arquivo inexistente")
+else:
+    # será executado se tudo ocorrer bem no try
+    print("arquivo manipulado e fechado com sucesso")
+    arquivo.close()
+finally:
+    # será sempre executado, independentemente de erro
+    print("Tentativa de abrir arquivo")
