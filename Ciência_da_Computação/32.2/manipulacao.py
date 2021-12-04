1aab2ac43223289c9aab62053c36d05e555c0a46
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-12-03 21:04:38 -0300

    escrita e leitura simples

---
+++ Ciência_da_Computação/32.2/manipulacao.py
@@ -0,0 +1,34 @@
+file = open("arquivo.txt", mode="w")
+# ao abrir um arquivo para escrita, um novo arquivo é criado mesmo que ele
+# já exista, sobrescrevendo o antigo.
+
+
+# file = open("arquivo.txt", mode="w")
+file.write("nome idade\n")
+file.write("Maria 45\n")
+file.write("Miguel 33\n")
+
+
+# Não precisa da quebra de linha, pois esse é um comportamento padrão do print
+print("Túlio 22", file=file)
+
+
+# Para escrever múltiplas linhas podemos utilizar a função writelines .
+# A função espera que cada linha tenha seu próprio caractere de separação \n
+LINES = ["Alberto 35\n", "Betina 22\n", "João 42\n", "Victor 19\n"]
+file.writelines(LINES)
+
+
+file.close()
+
+
+# escrita
+file = open("arquivo.txt", mode="w")
+file.write("Trybe S2")
+file.close()
+
+# leitura
+file = open("arquivo.txt", mode="r")
+content = file.read()
+print(content)
+file.close()  # não podemos esquecer de fechar o arquivo
--- Ciência_da_Computação/32.2/saidas.py
+++ Ciência_da_Computação/32.2/saidas.py
@@ -28,7 +28,8 @@ y = 3
 print(f"{x} / {y} = {x / y:.2f}")  # saída: 5 / 2 = 1.67
 # {x} é substituído por 5
 # {y} é substituído por 3
-# {x / y:.2f} O que vem após os dois pontos são formatadores, como nesse exemplo, duas casas decimais (.2f).
+# {x / y:.2f} O que vem após os dois pontos são formatadores,
+# como nesse exemplo, duas casas decimais (.2f).
 print(f"{x:.^3}")  # saída: ".5."
 # . é o caractere utilizado para preencher
 # ^ indica que o valor será centralizado
